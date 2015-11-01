f41eacadea1c5fe1f37c124c901c610a3fb6d016
Shane Li <shane95129@gmail.com>
2015-11-01 15:31:57 -0500

    Added User Class
    Implementation of User Class Finished

--- src/User.java
+++ src/User.java
@@ -1,6 +1,38 @@
 
 public class User {
-	
-	// TODO complete this class
+
+    private String username;
+    private String password;
+    SessionCookie cookie;
+
+    public User(String username, String password) {
+        this(username, password, null);
+    }
+
+    public User(String username, String password, SessionCookie cookie) {
+        this.username = username;
+        this.password = password;
+        this.cookie = cookie;
+    }
+
+    public String getName() {
+        return username;
+    }
+
+    public SessionCookie getCookie() {
+        return cookie;
+    }
+
+    public void setCookie(SessionCookie cookie) {
+        this.cookie = cookie;
+    }
+
+    public boolean checkPassword(String password) {
+        if (password.equals(this.password)) {
+            return true;
+        } else
+            return false;
+    }
+
 
 }
